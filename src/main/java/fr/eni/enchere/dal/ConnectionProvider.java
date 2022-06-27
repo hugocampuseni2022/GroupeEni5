001e83ff0680796cd2ba92f1d87425811470f7ae
lguitton2022 <lguitton2022@12SE14-BBMRZS2.ad.campus-eni.fr>
2022-06-27 12:19:59 +0200

    Modif DAL

--- src/main/java/fr/eni/enchere/dal/ConnectionProvider.java
+++ src/main/java/fr/eni/enchere/dal/ConnectionProvider.java
@@ -1,5 +1,31 @@
 package fr.eni.enchere.dal;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class ConnectionProvider {
+	
+	private static DataSource dataSource;
+	
+	static {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
+
+		} catch (NamingException exc ) {
+			exc.printStackTrace();
+			throw new RuntimeException("Connexion à la BD impossible");
+		}		
+	}
+	
+	public static Connection getConnection() throws SQLException
+	{
+		return dataSource.getConnection();
+	}
 
 }
