6bc265306e3eda8cb3530f703b5f20b4437e81ce
ben-ken0bi <97923580+ben-ken0bi@users.noreply.github.com>
2022-06-27 15:01:52 +0200

    connection utilisateur

--- src/main/java/fr/eni/enchere/bll/UtilisateurImpl.java
+++ src/main/java/fr/eni/enchere/bll/UtilisateurImpl.java
@@ -1,5 +1,56 @@
 package fr.eni.enchere.bll;
 
+import java.util.List;
+
+import fr.eni.enchere.bo.Utilisateur;
+import fr.eni.enchere.dal.DALException;
+import fr.eni.enchere.dal.DAOFactory;
+import fr.eni.enchere.dal.UtilisateurDAO;
+
 public class UtilisateurImpl implements UtilisateurManager{
+
+	private UtilisateurDAO utilisateurDao;
+
+	public UtilisateurImpl() {
+		utilisateurDao = DAOFactory.getDaoUtilisateur();
+	}
+
+
+
+	@Override
+	public Utilisateur connectionUtilisateur(String pseudo, String motDePasse) throws BLLException {
+		
+		try {
+			return utilisateurDao.connection(pseudo, motDePasse);
+		} catch (DALException e) {
+			throw new BLLException(e.getMessage(),e);
+		}
+	}
+	
+	/*
+	@Override
+	public List<Utilisateur> getAllUtilisateur() {
+		
+		return null;
+	}
+	
+	@Override
+	public void enregistrerUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void supprimerUtilisateur(int noUtilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Utilisateur getUtilisateurByNo(int noUtilisateur) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	*/
 
 }
