dce1918abe5b37409f85d6e53872298dca233672
ben-ken0bi <97923580+ben-ken0bi@users.noreply.github.com>
2022-06-27 14:03:32 +0200

    creation classe enchere

--- src/main/java/fr/eni/enchere/bo/Enchere.java
+++ src/main/java/fr/eni/enchere/bo/Enchere.java
@@ -1,7 +1,35 @@
 package fr.eni.enchere.bo;
 
+import java.time.LocalDate;
+
 public class Enchere {
 
+	// attributes
+	private LocalDate dateEnchere;
+	private int montant_Enchere;
 	
+	// construct
+	public Enchere(LocalDate dateEnchere, int montant_Enchere) {
+		super();
+		this.dateEnchere = dateEnchere;
+		this.montant_Enchere = montant_Enchere;
+	}
+
+	// getters and setters
+	public LocalDate getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(LocalDate dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public int getMontant_Enchere() {
+		return montant_Enchere;
+	}
+
+	public void setMontant_Enchere(int montant_Enchere) {
+		this.montant_Enchere = montant_Enchere;
+	}
 	
 }
