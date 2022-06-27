3485996fa7ad5b2d685d22f81769159134b45bb7
ben-ken0bi <97923580+ben-ken0bi@users.noreply.github.com>
2022-06-27 14:03:52 +0200

    creation classe categories

--- src/main/java/fr/eni/enchere/bo/Categorie.java
+++ src/main/java/fr/eni/enchere/bo/Categorie.java
@@ -7,7 +7,28 @@ public class Categorie {
 	private String libelle;
 	
 	// construct
-	
+	public Categorie(int noCategorie, String libelle) {
+		super();
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+	}
+
 	// getters and setters
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
 	
 }
