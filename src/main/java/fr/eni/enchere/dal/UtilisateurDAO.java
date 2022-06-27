6c9f9e3a8f8f53675b6f5c63a7dfe924924788ce
lguitton2022 <lguitton2022@12SE14-BBMRZS2.ad.campus-eni.fr>
2022-06-27 13:50:33 +0200

    Modification UtilisateurDAO

--- src/main/java/fr/eni/enchere/dal/UtilisateurDAO.java
+++ src/main/java/fr/eni/enchere/dal/UtilisateurDAO.java
@@ -1,7 +1,9 @@
 package fr.eni.enchere.dal;
 
+import fr.eni.enchere.bo.Utilisateur;
+
 public interface UtilisateurDAO {
 
-	
+	Utilisateur connection(String login, String mdp) throws DALException; 
 	
 }
--- src/main/java/fr/eni/enchere/dal/UtilisateurDAOImpl.java
+++ src/main/java/fr/eni/enchere/dal/UtilisateurDAOImpl.java
@@ -1,7 +1,39 @@
 package fr.eni.enchere.dal;
 
-public class UtilisateurDAOImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import fr.eni.enchere.bo.Utilisateur;
+
+public class UtilisateurDAOImpl implements UtilisateurDAO{
+
+	private final String SQL_CONNECTION = "SELECT * FROM UTILISATEURS WHERE (pseudo=? OR email=?) AND mot_de_passe=?";
+	
+	private static ResultSet rs;
+	private static PreparedStatement pStmt = null;
+	
+	@Override
+	public Utilisateur connection(String login, String mdp) throws DALException {
+		try (Connection connection= ConnectionProvider.getConnection()){
+			pStmt = connection.prepareStatement(SQL_CONNECTION);
+			
+			pStmt.setString(1, login);
+			pStmt.setString(2, login);
+			pStmt.setString(3, mdp);
+			
+			rs = pStmt.executeQuery();
+			
+			if (rs.next()) {
+				return new Utilisateur(rs.getInt("no_utilisateur"), rs.getInt("credit"), rs.getString("pseudo"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("telephone"), rs.getString("rue"), rs.getString("code_postal"), rs.getString("ville"), rs.getString("mot_de_passe"), rs.getBoolean("administrateur"));
+			}else {
+				throw new DALException("Login ou Mot de passe incorrecte");
+			}
+			
+		} catch (Exception e) {
+			throw new DALException("", e);
+		}
+	}
 	
 	
 }
