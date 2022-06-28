0ef472dc05c3143146969b42bfc7559932519760
htessier2022 <htessier2022@12SE14-JCMRZS2.ad.campus-eni.fr>
2022-06-28 11:15:37 +0200

    t

--- src/main/java/fr/eni/enchere/ihm/CreerCompteServlet.java
+++ src/main/java/fr/eni/enchere/ihm/CreerCompteServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.enchere.bll.BLLException;
 import fr.eni.enchere.bll.FactoryBLL;
 import fr.eni.enchere.bll.UtilisateurManager;
 import fr.eni.enchere.dal.DALException;
@@ -23,11 +24,11 @@ public class CreerCompteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	private UtilisateurManager UG;
-	private UtilisateurDAO UD;
+	
 	
 	@Override
 	public void init() throws ServletException {
-		UD = DAOFactory.getDaoUtilisateur();//Couplage faible !!!!
+		
 		UG = FactoryBLL.getManagerUtilisateur();
 		
 	}
@@ -53,15 +54,20 @@ public class CreerCompteServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-			
+			if (request.getParameter("mot de passe").equals(request.getParameter("Confirmation"))){
 			try {
-				("mot de passe").equals("Confirmation");
-				UD.insert(request.getParameter("pseudo"), request.getParameter("nom"), request.getParameter("prenom"), request.getParameter("mail"), request.getParameter("telephone"), request.getParameter("rue"), request.getParameter("code postal"), request.getParameter("ville"), request.getParameter("mot de passe"));
+				
+				UG.CreerCompteUtilisateur(request.getParameter("pseudo"), request.getParameter("nom"), request.getParameter("prenom"), request.getParameter("mail"), request.getParameter("telephone"), request.getParameter("rue"), request.getParameter("code postal"), request.getParameter("ville"), request.getParameter("mot de passe"));
 				request.getRequestDispatcher("/WEB-INF/pages/ListeEncheres.jsp").forward(request, response);
-			} catch (DALException e) {
-				request.setAttribute("error2", "Mots de Passe differents de la Confirmation");
-				request.getRequestDispatcher("/WEB-INF/pages/CreerCompte.jsp").forward(request, response);
+			
+			} catch (BLLException e) {
+				
 				e.printStackTrace();
+			}
+			}
+			else {
+				request.setAttribute("error2", "Mots de Passe differents de la Confirmation");
+				request.getRequestDispatcher("/WEB-INF/pages/CreerCompte.jsp").forward(request, response);	
 			}
 		
 				
